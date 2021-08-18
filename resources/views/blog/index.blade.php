8d4486d2d3fb6a42c6fc58c7f01d4058da6ba182
Roei Boribor <roeiboribor@gmail.com>
2021-08-18 17:21:58 +0800

    Add relation to user & post

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -21,8 +21,8 @@
         </h2>
         <span class="text-gray-500">
             By
-            <span class="font-bold italic text-gray-800">Roei Boribor</span>
-            , 1 day ago
+            <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>
+            , Created on {{ date('1S M Y', strtotime($post->updated_at)) }}
         </span>
         <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
             {{ $post->description }}
