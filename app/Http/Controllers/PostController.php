01b017c95b61cbdea600c7881fdc591c573cfa03
Roei Boribor <roeiboribor@gmail.com>
2021-08-19 21:06:53 +0800

    Add middleware to the routes on controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,13 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => [
+            'index',
+            'show'
+        ]]);
+    }
     /**
      * Display a listing of the resource.
      *
